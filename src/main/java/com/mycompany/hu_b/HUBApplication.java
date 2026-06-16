56ea135b180ed01f9258a5cbdb2eed3c6fadfa73
Anwar Permana <a.permana@qquest.nl>
2026-06-16 10:51:14 +0200

    springboot geinstalleerd

---
+++ src/main/java/com/mycompany/hu_b/HUBApplication.java
@@ -0,0 +1,14 @@
+package com.mycompany.hu_b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// Opstarten van springboot
+@SpringBootApplication
+public class HUBApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HUBApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/mycompany/hu_b/TestController.java
@@ -0,0 +1,13 @@
+package com.mycompany.hu_b;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Spring Boot werkt!";
+    }
+}
