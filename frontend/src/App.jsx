4bbf78fcca2b22be2552cd317457890406696600
Aiden <a.strijker@qquest.nl>
2026-06-17 13:39:27 +0200

    HU-B test logo + visuele chat voorbeelden toegevoegd

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,8 @@
 import { useState } from 'react';
+import qquestLogo from './assets/Qquest_Logo_Wit(RGB).png'
+import HUBIcon from './assets/rechthoekig-faceless-hub-logo-zwart.png'
+import './App.css'
+
 
 function App() {
   const [question, setQuestion] = useState('');
@@ -18,23 +22,63 @@ function App() {
   };
 
   return (
-    <div>
-      <h1>HU-B Chatbot</h1>
+    <>
+      <section id="header">
+        <div className="topBanner">
+          <img src={qquestLogo} className="qquestLogo" alt="Qquest logo" />
+          <img src={HUBIcon} className="HUBBanner" alt="HU-B logo" />
+          <h1 className="topText">De personeelsgids Chatbot</h1>
+        </div>          
+      </section>
+      
+      <section id="chat">
+        <h2>Antwoord (hier komt de chatgeschiedenis):</h2>
+        <div className="chatMessages">
+          <blockquote>{answer}</blockquote>
+        </div>
+      </section>
 
-      <input
-        type="text"
-        value={question}
-        onChange={(e) => setQuestion(e.target.value)}
-        placeholder="Stel een vraag..."
-      />
+      
+      <section id="input">
+        <input className="inputText"
+          type="text"
+          value={question}
+          onChange={(e) => setQuestion(e.target.value)}
+          placeholder="Stel een vraag..."
+        />
 
-      <button onClick={sendQuestion}>
-        Versturen
-      </button>
-
-      <h3>Antwoord:</h3>
-      <p>{answer}</p>
-    </div>
+        <button className="sendButton" onClick={sendQuestion}>
+          Versturen
+        </button>
+      </section>
+      <h1>Chat visuele playground</h1>
+      <div class="conversation">
+        <div class="message-group" data-sender="HU-B">
+          <blockquote>This would be the intro text of HU-B</blockquote>
+          <cite>HU-B</cite>
+        </div>
+        <div class="message-group" data-sender="User">
+          <blockquote>Here the user would ask a question</blockquote>
+          <cite>User</cite>
+        </div>
+        <div class="message-group" data-sender="HU-B">
+          <blockquote>HU-B mentions source</blockquote>
+          <blockquote>Actual answer you are looking for</blockquote>
+          <blockquote>Citation of answer.</blockquote>
+          <blockquote>Potential follow-up question.</blockquote>
+          <cite>HU-B</cite>
+        </div>
+        <div class="message-group" data-sender="User">
+          <blockquote>Next question of user</blockquote>
+          <cite>User</cite>
+        </div>
+        <div class="message-group" data-sender="HU-B">
+          <blockquote>…</blockquote>
+          <cite>HU-B</cite>
+        </div>
+      </div>
+      
+    </>
   );
 }
 
